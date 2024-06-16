bfdd66d825f303eba1b149a329ebefbcee5b40fc
rohanvarmma29 <rohangadddam95@gmail.com>
2024-06-16 16:48:54 +0530

    implemented useEffect hook and fetched live data from swiggy api

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,14 +1,43 @@
 import RestaurantCard from "./RestaurantCard";
 import resList from "../utils/mockData";
+import { useState, useEffect } from "react";
+
 
 
 const Body = ()=>{
+    //Local state variable - Super powerful variable
+    const [restaurantList, setRestaurantList] = useState([]);
+
+    useEffect(()=>{
+        fetchData();
+    },[]);
+
+    const fetchData = async ()=>{
+        const data = await fetch(
+            "https://www.swiggy.com/dapi/restaurants/list/v5?lat=17.37240&lng=78.43780&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+        );
+        const json = await data.json();
+        setRestaurantList(json.data.cards[1].card.card.gridElements.infoWithStyle.restaurants);
+
+    }
+
     return (
         <div className="body">
-            <div className="search">Search</div>
+            <div className="filter">
+                <button className="filter-btn"
+                    onClick={()=>{
+                        const filterdList = restaurantList.filter(
+                            (res)=>res.info.avgRating > 4.5
+                        );
+                        console.log(filterdList);
+                        setRestaurantList(filterdList);
+                    }}
+                >Top Rated Restaurants
+                </button>
+            </div>
             <div className="res-container">
                 {
-                    resList.map( restaurant=> <RestaurantCard resData={restaurant}/>)
+                    restaurantList.map( restaurant=> <RestaurantCard key={restaurant.info.id} resData={restaurant}/>)
                 }
             </div>
         </div>
